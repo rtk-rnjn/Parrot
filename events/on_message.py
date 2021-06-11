4b60948135f6df6003e138075ed3c9436d6e8d78
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-06-11 13:08:34 +0530

    Update on_message.py

--- events/on_message.py
+++ events/on_message.py
@@ -39,21 +39,21 @@ class SupportMail(Cog, name="Moderator Mail"):
 				mod_message = string[index:]
 				await member_object.send(f"> **Message from {message.author.name}#{message.author.discriminator}**\n```\n{mod_message}```\n\n> ID: {message.author.id}\n> AT: {datetime.utcnow()}")
 		
-		if message.channel.id == 833255204430020618:
-			
-			msg = message.content
-			key = 'MOvDyLB35FoAktTcorVUeCTS5'
-			if msg.startswith("$") or msg.startswith("<") or msg.startswith("!") or msg.startswith("-") or msg.startswith(";;") or msg.startswith("owo") or msg.startswith(","): return
-			params = {
-				"message": msg,
-				"key": key
-			}
-			req = requests.get(url="https://some-random-api.ml/chatbot", params=params)
-			if req.status_code != 200: return
-			res = req.json()
+		#if message.channel.id == 833255204430020618:
+		#	
+		#	msg = message.content
+		#	key = 'MOvDyLB35FoAktTcorVUeCTS5'  --EXPIRED--
+		#	if msg.startswith("$") or msg.startswith("<") or msg.startswith("!") or msg.startswith("-") or msg.startswith(";;") or msg.startswith("owo") or msg.startswith(","): return
+		#	params = {
+		#		"message": msg,
+		#		"key": key
+		#	}
+		#	req = requests.get(url="https://some-random-api.ml/chatbot", params=params)
+		#	if req.status_code != 200: return
+		#	res = req.json()
 
-			try: await self.bot.get_channel(833255204430020618).send(f"{message.author.mention} {res['response']}")
-			except: pass
+		#	try: await self.bot.get_channel(833255204430020618).send(f"{message.author.mention} {res['response']}")
+		#	except: pass
 
 
 def setup(bot):

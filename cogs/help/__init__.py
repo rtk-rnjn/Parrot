785c359837234f9e2b7f002c048d686935fc4bd6
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-08-06 15:20:08 +0000

    helping the help command

--- cogs/help/__init__.py
+++ cogs/help/__init__.py
@@ -32,21 +32,25 @@ class HelpCommand(commands.HelpCommand):
 
         get_bot = f"[Add me to your server]({INVITE})"
         support_server = f"[Support Server]({SUPPORT_SERVER})"
-        
+
         description = f"```\nPrefixes are '{self.context.prefix}' and '@Parrot#9209'\n```"
-        
+
         embed = discord.Embed(color=discord.Colour(0x55ddff))
-        
+
         embed.set_author(
             name=
             f"Server: {self.context.guild.name or self.context.author.name}",
             icon_url=self.context.guild.icon.url or self.context.me.avatar.url)
-        
+
         embed.description = description + f"\n• {get_bot}\n• {support_server}"
         embed.set_thumbnail(url=self.context.me.avatar.url)
         for cog in bot.cogs:
             if bot.cogs[cog].get_commands():
-                embed.add_field(name=f"{str(cog).upper()}", value=f"```\n{bot.cogs[cog].description if bot.cogs[cog].description else 'No help available :('}\n```", inline=False)
+                embed.add_field(
+                    name=f"{str(cog).upper()}",
+                    value=
+                    f"```\n{bot.cogs[cog].description if bot.cogs[cog].description else 'No help available :('}\n```",
+                    inline=False)
 
         embed.set_footer(text="Built with ❤️ and `discord.py`",
                          icon_url=f"{DEV_LOGO}")
@@ -78,10 +82,12 @@ class HelpCommand(commands.HelpCommand):
         cmds = list(group.commands)
 
         e = discord.Embed(
+            title=
+            f"Help with group **{group.name}{'|'.join(group.aliases) if group.aliases else ''}**",
             color=discord.Colour(0x55ddff),
             description=
             f"Sub commands\n```\n{', '.join([cmd.name for cmd in cmds])}\n```")
-        e.title = f"Help with group **{group.name}{'|'.join(group.aliases) if group.aliases else ''}**"
+
         e.set_footer(text="Built with ❤️ and `discord.py`",
                      icon_url=f"{DEV_LOGO}")
         e.set_thumbnail(url=self.context.me.avatar.url)
@@ -89,14 +95,14 @@ class HelpCommand(commands.HelpCommand):
 
         for cmd in cmds:
             e = discord.Embed(
-                title=cmd.name,
+                title=f"Help with {cmd.qualified_name}",
                 description=
                 f"```{cmd.help if cmd.help else 'No description.'}```\n"
                 f"Usage:\n```\n[p]{group.qualified_name}{'|'.join(group.aliases) if group.aliases else ''} {cmd.name}{'|' if cmd.aliases else ''}{'|'.join(cmd.aliases if cmd.aliases else 'NA')} {cmd.signature}\n```",
                 color=discord.Colour(0x55ddff))
             em_list.append(e)
             e.set_thumbnail(url=self.context.me.avatar.url)
-        
+
         paginator = Paginator(pages=em_list)
         await paginator.start(self.context)
 
@@ -107,7 +113,7 @@ class HelpCommand(commands.HelpCommand):
         embed = discord.Embed(
             title=f'{str(cog.qualified_name).capitalize()} Commands',
             description=
-            f"```\n{cog.description if cog.description else 'NA'}\n```",
+            f"```\n{cog.description if cog.description else 'NA'}\n```\nCommands\n```\n{', '.join([cmd.name for cmd in cog.get_commands()])}\n```",
             color=discord.Colour(0x55ddff))
         embed.set_footer(text="Built with ❤️ and `discord.py`",
                          icon_url=f"{DEV_LOGO}")

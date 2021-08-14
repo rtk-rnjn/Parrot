8d3806d4d9df0fdffff4289e1c383684872fb84f
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-08-14 23:46:52 +0530

    Source Command Fixed

--- cogs/mis.py
+++ cogs/mis.py
@@ -457,21 +457,14 @@ class misc(Cog, name="miscellaneous"):
             obj = self.bot.get_command(command.replace('.', ' '))
             if obj is None:
                 return await ctx.send('Could not find command.')
-
-            # since we found the command we're looking for, presumably anyway, let's
-            # try to access the code itself
             src = obj.callback.__code__
             module = obj.callback.__module__
             filename = src.co_filename
 
         lines, firstlineno = inspect.getsourcelines(src)
-        # if not module.startswith('discord'):
-        #     # not a built-in command
-        #     location = os.path.relpath(filename).replace('\\', '/')
-        # else:
-        #     location = module.replace('.', '/') + '.py'
-        #     source_url = 'https://github.com/Rapptz/discord.py'
-        #     branch = 'master'
+    
+        location = module.replace('.', '/') + '.py'
+        branch = 'master'
 
         final_url = f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
         await ctx.send(final_url)

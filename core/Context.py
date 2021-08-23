7b7f2111c3640deee3bd94bf5b838bce439637f0
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-08-23 22:10:26 +0530

    .

--- core/Context.py
+++ core/Context.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from discord.ext import commands
-import discord, typing
 
 __all__ = ("Context", )
 
@@ -8,19 +7,4 @@ __all__ = ("Context", )
 class Context(commands.Context):
     """A custom implementation of commands.Context class."""
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def __str__(self):
-        return "{0.__class__.__name__}".format(self)
-    
-    async def send(self, content: typing.Optional[str] = None, **kwargs) -> discord.Message:
-        if not self.channel.permissions_for(self.me).send_messages:
-            try:
-                await self.author.send(
-                    f"Bot can't send any messages in {self.channel.mention} channel. Please provide sufficient permissions."
-                )
-            except discord.Forbidden:
-                pass
-            return
-        async with self.channel.typing():
-            return await super().send(content, **kwargs)
+        super().__init__(**kwargs)

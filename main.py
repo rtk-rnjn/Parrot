fa66d90d6599f9829cf8031275e35b72b90be4f3
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-08-28 15:52:26 +0530

    changed something

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 VER = "v3.7.0"
 CHANGES = """
+- Fixed slap command
+- Users can report the other users
+- Be aware you can be banned from global-chat, once banned no chance of getting unbanned
 - Bug Fixes in snipe command
 - enable and disable command not woking for some reason
 - SECTOR 17-29 is now official support server

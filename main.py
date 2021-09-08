4fea725127d6342306d2958f05e2c12f4558c826
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-09-08 21:24:58 +0530

    ..

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from core import Parrot
 
 bot = Parrot()
 
-@bot.check
-async def check_ban(ctx):
-    if ctx.guild in None:
-        return False
-    else: 
-        return True
+# @bot.check
+# async def check_ban(ctx):
+#     if ctx.guild in None:
+#         return False
+#     else: 
+#         return True
 
 if __name__ == '__main__':
     bot.run()

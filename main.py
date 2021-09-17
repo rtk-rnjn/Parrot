358c996ff99d8e3559e9a341ea5a48fe470ddfa0
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-09-17 22:50:48 +0530

    ..

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from utilities.database import enable_disable
 from core import Parrot
 
 bot = Parrot()
@@ -12,7 +12,32 @@ async def bot_before_invoke(ctx):
 @bot.check
 async def can_run(ctx):
     if ctx.guild is not None:
-        
+        collection = enable_disable[f'{ctx.guild.id}']
+        if data := await collection.find_one({'_id': ctx.command.qualified_name}):
+            for role in ctx.author.roles:
+                if role.id in data['role_in']:
+                    return True
+                if role.id in data['role_out']:
+                    return False
+
+            if ctx.channel.id in data['channel_in']:
+                return True
+            
+            if ctx.channel.id in data['channel_out']:
+                return False
+            
+            if ctx.channel.category_id in data['cat_in']:
+                return True
+            
+            if ctx.channel.category_id in data['cat_out']:
+                return False
+
+            if data['server']:
+                return False
+            if not data['server']:
+                return True
+        return True
+    return False
 
 if __name__ == '__main__':
     bot.run()

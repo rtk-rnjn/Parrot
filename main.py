b810fc6afba82da5fa86029e374f37d4b92fe4c6
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-09-20 09:23:40 +0530

    ..

--- main.py
+++ main.py
@@ -16,10 +16,16 @@ async def can_run(ctx):
         # if is_owner: return True
         collection = enable_disable[f'{ctx.guild.id}']
         if data := await collection.find_one({'_id': ctx.command.qualified_name}):
-            if ctx.channel.id in data['channel_in']: return True
+            if ctx.channel.id in data['channel_in']: 
+                print(1)
+                return True
             for role in ctx.author.roles:
-                if role.id in data['role_in']: return True
-                if role.id in data['role_out']: return False
+                if role.id in data['role_in']: 
+                    print(2)
+                    return True
+                if role.id in data['role_out']: 
+                    print(3)
+                    return False
             if ctx.channel.id in data['channel_out']: return False
             if data['server']: return False
             if not data['server']: return True

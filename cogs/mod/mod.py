91eacf275128d14d710d4ebcc8c846ad4c314d75
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-09-20 10:01:05 +0530

    ..

--- cogs/mod/mod.py
+++ cogs/mod/mod.py
@@ -10,7 +10,7 @@ from datetime import datetime
 collection = parrot_db['server_config']
 
 
-class mod(Cog):
+class moderation(Cog):
     """A simple moderator's tool for managing the server."""
     def __init__(self, bot: Parrot):
         self.bot = bot

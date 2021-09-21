bee53151849e056747e8c6f4291090cd3b3a77ca
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-09-21 20:09:15 +0530

    ..

--- main.py
+++ main.py
@@ -23,11 +23,15 @@ async def can_run(ctx):
                     return True
                 if role.id in data['role_out']: 
                     return False
-            if ctx.channel.id in data['channel_out']: return False
-            if data['server']: return False
-            if not data['server']: return True
+            if ctx.channel.id in data['channel_out']: 
+                return False
+            if data['server']: 
+                return False
+            if not data['server']: 
+                return True
         if data := await collection.find_one({'_id': ctx.command.cog.qualified_name}):
-            if ctx.channel.id in data['channel_in']: return True
+            if ctx.channel.id in data['channel_in']: 
+                return True
             for role in ctx.author.roles:
                 if role.id in data['role_in']: return True
                 if role.id in data['role_out']: return False

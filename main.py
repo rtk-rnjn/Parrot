43306d89a52be3974c2490edd700e035b82d7e10
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-09-30 14:58:50 +0000

    Just a bit more checks

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from core import Parrot
 
 bot = Parrot()
 
+
 @bot.before_invoke
 async def bot_before_invoke(ctx):
     if ctx.guild is not None:
         if not ctx.guild.chunked:
             await ctx.guild.chunk()
 
+
 @bot.check
 async def bot_check(ctx):
-    _true = await _can_run(ctx)
-    return _true
+    if ctx.command is not None:
+        _true = await _can_run(ctx)
+        return _true
+
 
 if __name__ == '__main__':
     bot.run()

f7a0b41292647bd9f5a70e5698c7d6321e7ce057
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-10-10 23:36:09 +0530

    Update on_rexn.py

--- events/on_rexn.py
+++ events/on_rexn.py
@@ -25,29 +25,7 @@ class OnReaction(Cog, command_attrs=dict(hidden=True)):
 
     @Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        if data := await collection.find_one({'_id': payload.guild_id}):
-            try:
-                if data['star_lock']:
-                    return
-            except Exception:
-                pass
-            try:
-                starboard = data['starboard']
-            except KeyError:
-                return
-            if starboard['emoji'] == str(reaction.emoji) and starboard['count'] == reaction.count:
-                channel = self.bot.get_channel(starboard['channel'])
-                if not channel:
-                    return
-                else:
-                    embed = discord.Embed(
-                        title=f"{reaction.message.author}", url=f"{reaction.message.jump_url}",
-                        description=message.content, timestamp=datetime.utcnow(), color=reaction.message.author.color
-                    ).set_footer(text=f"{reaction.message.guild.name}", icon_url=reaction.message.author.display_avatar.url)
-                    msg = await channel.send(content=f'Star Count: {starboard["count"]}', embed=embed)
-                    self.message_cache[reaction.message.id] = {'emoji': starboard['emoji'], 'count': starboard['count']}
-                    self.msg_obj[reaction.message.id] = msg
-                    await msg.add_reaction(starboard['emoji'])
+        pass
                     
     @Cog.listener()
     async def on_raw_reaction_add(self, payload):
@@ -151,16 +129,7 @@ class OnReaction(Cog, command_attrs=dict(hidden=True)):
 
     @Cog.listener()
     async def on_reaction_remove(self, reaction, user):
-        try:
-            data = self.message_cache[reaction.message.id]
-        except KeyError:
-            return
-        finally:
-            if (reaction.message.emoji) == data['emoji'] and (data['count'] > reaction.count):
-                try:
-                    await self.msg_obj[reaction.message.id].delete()
-                except Exception:
-                    pass
+        pass
 
     @Cog.listener()
     async def on_raw_reaction_remove(self, payload):

ed56bba2eb433bcfef6702aabeef26c6f6e6e2a0
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-10-12 16:01:14 +0530

    Update join.py

--- events/guild/join.py
+++ events/guild/join.py
@@ -10,13 +10,10 @@ BASE_URL = "https://discord.com/api/webhooks/883368274501460039/"
 class GuildJoin(Cog, command_attrs=dict(hidden=True)):
     def __init__(self, bot: Parrot):
         self.bot = bot
-        self.channel = None
         self.url = BASE_URL + os.environ['CHANNEL_TOKEN1']
 
     @Cog.listener()
     async def on_guild_join(self, guild):
-        if self.channel is None:
-            self.channel = self.bot.get_channel(883368274501460039)
         CONTENT = f"Joined {guild.name} ({guild.id}). Total member in {guild.name}: {guild.member_count}. Server Owner: {guild.owner} ({guild.owner.id}). Server Region: {str(guild.region).replace('_', ' ').title()}. \n\nTotal server on count {len(self.bot.guilds)}. Total users on count: {len(self.bot.users)}"
         await guild_join(guild.id)
         data = {
@@ -28,13 +25,9 @@ class GuildJoin(Cog, command_attrs=dict(hidden=True)):
             async with session.post(self.url, json=data) as res:
                 if res in range(200, 300 + 1):
                     pass
-                else:
-                    await self.channel.send(CONTENT)
 
     @Cog.listener()
     async def on_guild_remove(self, guild):
-        if self.channel is None:
-            self.channel = self.bot.get_channel(883368274501460039)
         CONTENT = f"Left {guild.name} ({guild.id}). Total member in {guild.name}: {guild.member_count}. Server Owner: {guild.owner} ({guild.owner.id}). Server Region: {str(guild.region).replace('_', ' ').title()}. \n\nTotal server on count {len(self.bot.guilds)}. Total users on count: {len(self.bot.users)}"
         await guild_remove(guild.id)
         data = {
@@ -46,8 +39,6 @@ class GuildJoin(Cog, command_attrs=dict(hidden=True)):
             async with session.post(self.url, json=data) as res:
                 if res in range(200, 300 + 1):
                     pass
-                else:
-                    await self.channel.send(CONTENT)
 
     @Cog.listener()
     async def on_guild_update(self, before, after):

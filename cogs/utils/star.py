6c6d7c9de7a6ac9fc870ec1505c55030cdb53c74
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-10-23 11:56:51 +0530

    Update star.py

--- cogs/utils/star.py
+++ cogs/utils/star.py
@@ -96,7 +96,7 @@ class Stars(Cog):
     @cache.cache()
     async def get_starboard(self, guild_id, *, collection=None):
         collection = collection or parrot_db['server_config']
-        if data := await collection.find_one({'_id': guild_id}):
+        if data := await collection.find_one({'_id': guild_id, 'starboard': {}}):
             return StarboardConfig(guild_id=guild_id, bot=self.bot, record=data)
         else:
             await collection.update_one(

1bf2571301fa6f94df1ef392fc06d095fa984144
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-10-27 22:24:18 +0530

    debuggin

--- cogs/mod/spam_prot.py
+++ cogs/mod/spam_prot.py
@@ -13,10 +13,11 @@ with open('extra/duke_nekum.txt') as f:
 
 class SpamProt(Cog):
     def __init__(self, bot: Parrot):
+        print('loaded')
         self.bot = bot
         self.collection = parrot_db['server_config']
         self.cd_mapping = commands.CooldownMapping.from_cooldown(
-            10, 10, commands.BucketType.member)
+            5, 5, commands.BucketType.member)
         
     async def delete(self, message: discord.Message, *, reason: str=None) -> None:
         def check(m: discord.Message):
@@ -30,21 +31,30 @@ class SpamProt(Cog):
     async def on_message(self, message: discord.Message):
         if message.author.bot or (not message.guild):
             return
+        print(1)
         if message.author.guild_permissions.administrator:
             if message.author.id == 741614468546560092:
+                print(2)
                 pass
             else:
+                print(3)
                 return
         bucket = self.cd_mapping.get_bucket(message)
+        print(4)
         retry_after = bucket.update_rate_limit()
+        print(5, retry_after)
         if retry_after:
+            print(6)
             if data := await self.collection.find_one({'_id': message.guild.id, 'automod.spam.enable': {'$exists': True}}):
+                print(7)
                 if not data['automod']['spam']['enable']:
+                    print(8)
                     return
                 try:
                     ignore = data['automod']['spam']['channel']
                 except KeyError:
                     ignore = []
+                
                 if message.channel.id in ignore:
                     return
                 await self.delete(message)

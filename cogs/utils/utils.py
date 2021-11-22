f98904bb73790c8c6a0bb477f7fe6431beb93e5c
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-11-22 19:53:10 +0530

    typo

--- cogs/utils/utils.py
+++ cogs/utils/utils.py
@@ -177,7 +177,7 @@ class Utils(Cog):
 
     @tasks.loop(seconds=1)
     async def gw_tasks(self):
-        await self.bot.wait_until_read()
+        await self.bot.wait_until_ready()
         async for data in giveaway.find({'endtime': {'$lte': datetime.datetime.utcnow().timestamp()}}):
             print('loop executed')
             print(data)

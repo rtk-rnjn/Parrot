2a81cc123e0998ac331b0fe628525f144871ccbe
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-11-23 23:23:46 +0530

    Fixed: `on_guild_channel_delete`

--- events/guild/channel.py
+++ events/guild/channel.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from core import Cog, Parrot
 from utilities.database import parrot_db
 
-import discord, time
+import discord, time, io, json
 
 log = parrot_db['logging']
 
@@ -13,17 +13,35 @@ class GuildChannel(Cog, command_attrs=dict(hidden=True)):
         self.bot = bot
         self.collection = log
 
+    @staticmethod
+    def _overwrite_to_json(overwrites):
+        try:
+            over = {
+                f"{str(target.name)} ({'Role' if type(target) is discord.Role else 'Member'})": overwrite._values for target, overwrite in overwrites.items()
+            }
+            return json.dumps(over, indent=4)
+        except Exception:
+            return "{}"
+    
     @Cog.listener()
     async def on_guild_channel_delete(self, channel: discord.TextChannel):
         if data := await self.collection.find_one({'_id': channel.guild.id, 'on_channel_delete': {'$exists': True}}):
             webhook = discord.Webhook(data['on_channel_delete'], session=self.bot.session, bot_token=self.bot.http.token)
-            embed = discord.Embed(title='Channel Delete', color=self.bot.color)
+            embed = discord.Embed(title='Channel Delete Event', color=self.bot.color)
             data = [
                 ("Name", channel.name),
                 ("Created at", f"<t:{int(channel.created_at.timestamp())}>"),
                 ("Position", channel.position)
             ]
-            await webhook.send(content=f"**Channel Delete Event at: <t:{int(time.time())}>**", avatar_url=self.bot.user.avatar.url, username=self.bot.user.name)
+            for name, value in data: embed.add_field(name=name, value=value, inline=True)
+            embed.set_footer(text=f"ID: {channel.id}")
+            fp = io.BytesIO(self._overwrite_to_dict(channel.overwrites).encode())
+            await webhook.send(
+                content=f"**Channel Deleted at: <t:{int(time.time())}>**", 
+                avatar_url=self.bot.user.avatar.url, 
+                username=self.bot.user.name,
+                file=discord.File(fp, filename='overwrites.json')
+            )
 
     @Cog.listener()
     async def on_guild_channel_create(self, channel):

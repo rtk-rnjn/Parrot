37cfcb62e577c48249d746dc9a25463fc703d598
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-12-14 22:01:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+from __future__ import annotations
 
 from core import Parrot, Context
 
 bot = Parrot()
-
 
 @bot.before_invoke
 async def bot_before_invoke(ctx: Context):
     if ctx.guild:
         if not ctx.guild.chunked:
             await ctx.guild.chunk()
-
 
 if __name__ == '__main__':
     bot.run()

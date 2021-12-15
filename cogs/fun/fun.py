aab7af6a9af2bebca14004a447f072b2490a1095
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-12-15 10:06:38 +0530

    Add hangman commands

--- cogs/fun/fun.py
+++ cogs/fun/fun.py
@@ -35,6 +35,18 @@ def suppress_links(message: str) -> str:
         message = message.replace(link, f"<{link}>")
     return message
 
+ALL_WORDS = Path("extra/hangman_words.txt").read_text().splitlines()
+
+IMAGES = {
+    6: "https://cdn.discordapp.com/attachments/859123972884922418/888133201497837598/hangman0.png",
+    5: "https://cdn.discordapp.com/attachments/859123972884922418/888133595259084800/hangman1.png",
+    4: "https://cdn.discordapp.com/attachments/859123972884922418/888134194474139688/hangman2.png",
+    3: "https://cdn.discordapp.com/attachments/859123972884922418/888133758069395466/hangman3.png",
+    2: "https://cdn.discordapp.com/attachments/859123972884922418/888133786724859924/hangman4.png",
+    1: "https://cdn.discordapp.com/attachments/859123972884922418/888133828831477791/hangman5.png",
+    0: "https://cdn.discordapp.com/attachments/859123972884922418/888133845449338910/hangman6.png",
+}
+
 
 response = ["All signs point to yes...",
             "Yes!", 
@@ -378,6 +390,24 @@ class Fun(Cog, command_attrs={'cooldown': commands.CooldownMapping.from_cooldown
     def cog_unload(self) -> None:
         """Cancel `get_wiki_questions` task when Cog will unload."""
         self.get_wiki_questions.cancel()
+    
+    @staticmethod
+    def create_embed(tries: int, user_guess: str) -> Embed:
+        """
+        Helper method that creates the embed where the game information is shown.
+        This includes how many letters the user has guessed so far, and the hangman photo itself.
+        """
+        hangman_embed = Embed(
+            title="Hangman",
+            color=Colours.python_blue,
+        )
+        hangman_embed.set_image(url=IMAGES[tries])
+        hangman_embed.add_field(
+            name=f"You've guessed `{user_guess}` so far.",
+            value="Guess the word by sending a message with a letter!"
+        )
+        hangman_embed.set_footer(text=f"Tries remaining: {tries}")
+        return hangman_embed
 
     @property
     def display_emoji(self) -> discord.PartialEmoji:
@@ -615,17 +645,21 @@ class Fun(Cog, command_attrs={'cooldown': commands.CooldownMapping.from_cooldown
 
                 await ctx.send(embed=embed)
 
-            def check_func(variation_tolerance: int) -> Callable[[discord.Message], bool]:
-                def contains_correct_answer(m: discord.Message) -> bool:
-                    return m.channel == ctx.channel and any(
-                        fuzz.ratio(answer.lower(), m.content.lower()) > variation_tolerance
-                        for answer in quiz_entry.answers
-                    )
+            # def check_func(variation_tolerance: int) -> Callable[[discord.Message], bool]:
+            #     def contains_correct_answer(m: discord.Message) -> bool:
+            #         return (m.channel == ctx.channel) and any(
+            #             fuzz.ratio(answer.lower(), m.content.lower()) > variation_tolerance
+            #             for answer in quiz_entry.answers
+            #         )
 
-                return contains_correct_answer
-
+            #     return contains_correct_answer
+            
+            def check(m: discord.Message) -> bool:
+                return (m.channel.id == ctx.channel.id) and any(
+                    fuzz.ratio(answer.lower(), m.content.lower()) > quiz_entry.var_tol for answer in quiz_entry.answers)
+            
             try:
-                msg = await self.bot.wait_for("message", check=check_func(quiz_entry.var_tol), timeout=10)
+                msg = await self.bot.wait_for("message", check=check, timeout=10)
             except asyncio.TimeoutError:
                 # In case of TimeoutError and the game has been stopped, then do nothing.
                 if not self.game_status[ctx.channel.id]:
@@ -722,6 +756,129 @@ class Fun(Cog, command_attrs={'cooldown': commands.CooldownMapping.from_cooldown
         )
 
         return error_embed
+
+    @commands.command()
+    async def hangman(
+            self,
+            ctx: commands.Context,
+            min_length: int = 0,
+            max_length: int = 25,
+            min_unique_letters: int = 0,
+            max_unique_letters: int = 25,
+    ) -> None:
+        """
+        Play hangman against the bot, where you have to guess the word it has provided!
+        The arguments for this command mean:
+        - min_length: the minimum length you want the word to be (i.e. 2)
+        - max_length: the maximum length you want the word to be (i.e. 5)
+        - min_unique_letters: the minimum unique letters you want the word to have (i.e. 4)
+        - max_unique_letters: the maximum unique letters you want the word to have (i.e. 7)
+        """
+        # Filtering the list of all words depending on the configuration
+        filtered_words = [
+            word for word in ALL_WORDS
+            if min_length < len(word) < max_length
+            and min_unique_letters < len(set(word)) < max_unique_letters
+        ]
+
+        if not filtered_words:
+            filter_not_found_embed = Embed(
+                title=choice(NEGATIVE_REPLIES),
+                description="No words could be found that fit all filters specified.",
+                color=Colours.soft_red,
+            )
+            await ctx.send(embed=filter_not_found_embed)
+            return
+
+        word = choice(filtered_words)
+        # `pretty_word` is used for comparing the indices where the guess of the user is similar to the word
+        # The `user_guess` variable is prettified by adding spaces between every dash, and so is the `pretty_word`
+        pretty_word = ''.join([f"{letter} " for letter in word])[:-1]
+        user_guess = ("_ " * len(word))[:-1]
+        tries = 6
+        guessed_letters = set()
+
+        def check(msg: discord.Message) -> bool:
+            return msg.author == ctx.author and msg.channel == ctx.channel
+
+        original_message = await ctx.send(embed=Embed(
+            title="Hangman",
+            description="Loading game...",
+            color=Colours.soft_green
+        ))
+
+        # Game loop
+        while user_guess.replace(' ', '') != word:
+            # Edit the message to the current state of the game
+            await original_message.edit(embed=self.create_embed(tries, user_guess))
+
+            try:
+                message = await self.bot.wait_for(
+                    event="message",
+                    timeout=60.0,
+                    check=check
+                )
+            except TimeoutError:
+                timeout_embed = Embed(
+                    title=choice(NEGATIVE_REPLIES),
+                    description="Looks like the bot timed out! You must send a letter within 60 seconds.",
+                    color=Colours.soft_red,
+                )
+                await original_message.edit(embed=timeout_embed)
+                return
+
+            # If the user enters a capital letter as their guess, it is automatically converted to a lowercase letter
+            normalized_content = message.content.lower()
+            # The user should only guess one letter per message
+            if len(normalized_content) > 1:
+                letter_embed = Embed(
+                    title=choice(NEGATIVE_REPLIES),
+                    description="You can only send one letter at a time, try again!",
+                    color=Colours.dark_green,
+                )
+                await ctx.send(embed=letter_embed, delete_after=4)
+                continue
+
+            # Checks for repeated guesses
+            elif normalized_content in guessed_letters:
+                already_guessed_embed = Embed(
+                    title=choice(NEGATIVE_REPLIES),
+                    description=f"You have already guessed `{normalized_content}`, try again!",
+                    color=Colours.dark_green,
+                )
+                await ctx.send(embed=already_guessed_embed, delete_after=4)
+                continue
+
+            # Checks for correct guesses from the user
+            elif normalized_content in word:
+                positions = {idx for idx, letter in enumerate(pretty_word) if letter == normalized_content}
+                user_guess = "".join(
+                    [normalized_content if index in positions else dash for index, dash in enumerate(user_guess)]
+                )
+
+            else:
+                tries -= 1
+
+                if tries <= 0:
+                    losing_embed = Embed(
+                        title="You lost.",
+                        description=f"The word was `{word}`.",
+                        color=Colours.soft_red,
+                    )
+                    await original_message.edit(embed=self.create_embed(tries, user_guess))
+                    await ctx.send(embed=losing_embed)
+                    return
+
+            guessed_letters.add(normalized_content)
+
+        # The loop exited meaning that the user has guessed the word
+        await original_message.edit(embed=self.create_embed(tries, user_guess))
+        win_embed = Embed(
+            title="You won!",
+            description=f"The word was `{word}`.",
+            color=Colours.grass_green
+        )
+        await ctx.send(embed=win_embed)
 
     @quiz_game.command(name="stop")
     async def stop_quiz(self, ctx: commands.Context) -> None:

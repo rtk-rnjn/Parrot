bcd30fd3eb986437d134e0d8a99e949b65fbd9db
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-12-16 13:13:18 +0530

    Emoji prob

--- interactions/buttons/games.py
+++ interactions/buttons/games.py
@@ -39,15 +39,15 @@ class Emojis:
 
   
     number_emojis = {
-        1: "\u0031\ufe0f\u20e3",
-        2: "\u0032\ufe0f\u20e3",
-        3: "\u0033\ufe0f\u20e3",
-        4: "\u0034\ufe0f\u20e3",
-        5: "\u0035\ufe0f\u20e3",
-        6: "\u0036\ufe0f\u20e3",
-        7: "\u0037\ufe0f\u20e3",
-        8: "\u0038\ufe0f\u20e3",
-        9: "\u0039\ufe0f\u20e3"
+        1: "\N{DIGIT ONE}",
+        2: "\N{DIGIT TWO}",
+        3: "\N{DIGIT THREE}",
+        4: "\N{DIGIT FOUR}",
+        5: "\N{DIGIT FIVE}",
+        6: "\N{DIGIT SIX}",
+        7: "\N{DIGIT SEVEN}",
+        8: "\N{DIGIT EIGHT}",
+        9: "\N{DIGIT NINE}"
     }
 
     confirmation = "\u2705"

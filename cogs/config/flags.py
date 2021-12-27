ce7c175b7cf361a3bd6de4432b40e734f4dcc4d1
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-12-27 20:02:03 +0530

    Flags and upgrades

---
+++ cogs/config/flags.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from discord.ext import commands
+from typing import Optional
+
+from utilities.time import ShortTime
+from utilities.converters import convert_bool
+
+class AutoWarn(commands.FlagConverter, case_insensitive=True, delimiter=' ', prefix='--'):
+    enable: Optional[convert_bool] = True
+    count: Optional[int]
+    punish: Optional[str]
+    duration: Optional[ShortTime]
+    delete: Optional[convert_bool] = True

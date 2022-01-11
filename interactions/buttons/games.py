81813d0db1c7dbf6b4fb7e97b306e4302407a54c
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-01-11 09:49:32 +0530

    nice

--- interactions/buttons/games.py
+++ interactions/buttons/games.py
@@ -2562,12 +2562,7 @@ class GameBattleShip:
             for message in alert_messages:
                 await message.delete()
 
-            alert_messages = []
-            alert_messages.append(
-                await self.next.user.send(
-                    f"{self.turn.user} aimed at {self.match.string}!"
-                )
-            )
+            alert_messages = [await self.next.user.send(f"{self.turn.user} aimed at {self.match.string}!")]
 
             if square.boat:
                 await self.hit(square, alert_messages)

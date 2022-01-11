cd3e82c85d50db478f552c1ce78b5a9a708942de
Ritik Ranjan <ritik0ranjan@gmail.com>
2022-01-11 15:23:04 +0530

    Update _snakes_cog.py

--- interactions/buttons/snakes/_snakes_cog.py
+++ interactions/buttons/snakes/_snakes_cog.py
@@ -154,10 +154,6 @@ CARD = {
 
 async def invoke_help_command(ctx: Context) -> None:
     """Invoke the help command or default help command if help extensions is not loaded."""
-    if "bot.exts.core.help" in ctx.bot.extensions:
-        help_command = ctx.bot.get_command("help")
-        await ctx.invoke(help_command, ctx.command.qualified_name)
-        return
     await ctx.send_help(ctx.command)
 
 

f0b036b13114433757b91d98a11793d78a264bf9
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-01-12 14:33:08 +0530

    Commit

---
+++ cogs/holidays/hanukkah/__init__.py
@@ -0,0 +1,98 @@
+from __future__ import annotations
+
+import datetime
+
+from discord import Embed
+from discord.ext import commands
+
+from core import Cog, Context, Parrot
+from utilities.constants import Colours, Month
+from utilities.deco import in_month
+HEBCAL_URL = (
+    "https://www.hebcal.com/hebcal/?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&"
+    "year=now&month=x&ss=on&mf=on&c=on&geo=geoname&geonameid=3448439&m=50&s=on"
+)
+
+
+class HanukkahEmbed(Cog):
+    """A cog that returns information about Hanukkah festival."""
+
+    def __init__(self, bot: Parrot):
+        self.bot = bot
+        self.hanukkah_dates: list[datetime.date] = []
+
+    def _parse_time_to_datetime(self, date: list[str]) -> datetime.datetime:
+        """Format the times provided by the api to datetime forms."""
+        try:
+            return datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S%z")
+        except ValueError:
+            # there is a possibility of an event not having a time, just a day
+            # to catch this, we try again without time information
+            return datetime.datetime.strptime(date, "%Y-%m-%d")
+
+    async def fetch_hanukkah_dates(self) -> list[datetime.date]:
+        """Gets the dates for hanukkah festival."""
+        # clear the datetime objects to prevent a memory link
+        self.hanukkah_dates = []
+        async with self.bot.http_session.get(HEBCAL_URL) as response:
+            json_data = await response.json()
+        festivals = json_data["items"]
+        for festival in festivals:
+            if festival["title"].startswith("Chanukah"):
+                date = festival["date"]
+                self.hanukkah_dates.append(self._parse_time_to_datetime(date).date())
+        return self.hanukkah_dates
+
+    @in_month(Month.NOVEMBER, Month.DECEMBER)
+    @commands.command(name="hanukkah", aliases=("chanukah",))
+    async def hanukkah_festival(self, ctx: Context) -> None:
+        """Tells you about the Hanukkah Festivaltime of festival, festival day, etc)."""
+        hanukkah_dates = await self.fetch_hanukkah_dates()
+        start_day = hanukkah_dates[0]
+        end_day = hanukkah_dates[-1]
+        today = datetime.date.today()
+        embed = Embed(title="Hanukkah", colour=Colours.blue)
+        if start_day <= today <= end_day:
+            if start_day == today:
+                now = datetime.datetime.utcnow()
+                hours = now.hour + 4  # using only hours
+                hanukkah_start_hour = 18
+                if hours < hanukkah_start_hour:
+                    embed.description = (
+                        "Hanukkah hasnt started yet, "
+                        f"it will start in about {hanukkah_start_hour - hours} hour/s."
+                    )
+                    await ctx.send(embed=embed)
+                    return
+                elif hours > hanukkah_start_hour:
+                    embed.description = (
+                        "It is the starting day of Hanukkah! "
+                        f"Its been {hours - hanukkah_start_hour} hours hanukkah started!"
+                    )
+                    await ctx.send(embed=embed)
+                    return
+            festival_day = hanukkah_dates.index(today)
+            number_suffixes = ["st", "nd", "rd", "th"]
+            suffix = number_suffixes[festival_day - 1 if festival_day <= 3 else 3]
+            message = ":menorah:" * festival_day
+            embed.description = (
+                f"It is the {festival_day}{suffix} day of Hanukkah!\n{message}"
+            )
+        elif today < start_day:
+            format_start = start_day.strftime("%d of %B")
+            embed.description = (
+                "Hanukkah has not started yet. "
+                f"Hanukkah will start at sundown on {format_start}."
+            )
+        else:
+            format_end = end_day.strftime("%d of %B")
+            embed.description = (
+                "Looks like you missed Hanukkah! "
+                f"Hanukkah ended on {format_end}."
+            )
+
+        await ctx.send(embed=embed)
+
+def setup(bot: Parrot) -> None:
+    """Load the Hanukkah Embed Cog."""
+    bot.add_cog(HanukkahEmbed(bot))
---
+++ cogs/holidays/pride/__init__.py
@@ -0,0 +1,209 @@
+import json
+import logging
+import random
+import discord
+
+from discord.ext import commands
+
+from pathlib import Path
+from datetime import datetime
+from rapidfuzz import fuzz
+import dateutil.parser
+from typing import Optional, Union
+
+from utilities.deco import seasonal_task
+from utilities.constants import Month, Colours
+
+from core import Cog, Context, Parrot
+
+NAMES = json.loads(Path(r"extra/pride/drag_queen_names.json").read_text("utf8"))
+VIDEOS = json.loads(Path(r"extra/pride/anthems.json").read_text("utf8"))
+FACTS = json.loads(Path(r"extra/pride/facts.json").read_text("utf8"))
+
+PRIDE_RESOURCE = json.loads(Path(r"extra/pride/prideleader.json").read_text("utf8"))
+MINIMUM_FUZZ_RATIO = 40
+
+
+class Pride(Cog):
+    """PRIDE PRIDE PRIDE!"""
+    def __init__(self, bot: Parrot):
+        self.bot = bot
+        self.daily_fact_task = self.bot.loop.create_task(self.send_pride_fact_daily())
+    
+    @seasonal_task(Month.JUNE)
+    async def send_pride_fact_daily(self) -> None:
+        """Background task to post the daily pride fact every day."""
+        await self.bot.wait_until_guild_available()
+
+        channel = self.bot.get_channel(Channels.community_bot_commands)
+        await self.send_select_fact(channel, datetime.utcnow())
+
+    async def send_random_fact(self, ctx: Context) -> None:
+        """Provides a fact from any previous day, or today."""
+        now = datetime.utcnow()
+        previous_years_facts = (y for x, y in FACTS.items() if int(x) < now.year)
+        current_year_facts = FACTS.get(str(now.year), [])[:now.day]
+        previous_facts = current_year_facts + [x for y in previous_years_facts for x in y]
+        try:
+            await ctx.send(embed=self.make_embed(random.choice(previous_facts)))
+        except IndexError:
+            await ctx.send("No facts available")
+
+    async def send_select_fact(self, target: discord.abc.Messageable, _date: Union[str, datetime]) -> None:
+        """Provides the fact for the specified day, if the day is today, or is in the past."""
+        now = datetime.utcnow()
+        if isinstance(_date, str):
+            try:
+                date = dateutil.parser.parse(_date, dayfirst=False, yearfirst=False, fuzzy=True)
+            except (ValueError, OverflowError) as err:
+                await target.send(f"Error parsing date: {err}")
+                return
+        else:
+            date = _date
+        if date.year < now.year or (date.year == now.year and date.day <= now.day):
+            try:
+                await target.send(embed=self.make_embed(FACTS[str(date.year)][date.day - 1]))
+            except KeyError:
+                await target.send(f"The year {date.year} is not yet supported")
+                return
+            except IndexError:
+                await target.send(f"Day {date.day} of {date.year} is not yet support")
+                return
+        else:
+            await target.send("The fact for the selected day is not yet available.")
+
+    @commands.command(name="pridefact", aliases=("pridefacts",))
+    async def pridefact(self, ctx: Context, option: str = None) -> None:
+        """
+        Sends a message with a pride fact of the day.
+        If "random" is given as an argument, a random previous fact will be provided.
+        If a date is given as an argument, and the date is in the past, the fact from that day
+        will be provided.
+        """
+        if not option:
+            await self.send_select_fact(ctx, datetime.utcnow())
+        elif option.lower().startswith("rand"):
+            await self.send_random_fact(ctx)
+        else:
+            await self.send_select_fact(ctx, option)
+
+    @staticmethod
+    def make_embed(fact: str) -> discord.Embed:
+        """Makes a nice embed for the fact to be sent."""
+        return discord.Embed(
+            colour=Colours.pink,
+            title="Pride Fact!",
+            description=fact
+        )
+    @commands.command(name="dragname", aliases=("dragqueenname", "queenme"))
+    async def dragname(self, ctx: Context) -> None:
+        """Sends a message with a drag queen name."""
+        await ctx.send(random.choice(NAMES))
+    def get_video(self, genre: Optional[str] = None) -> dict:
+        """
+        Picks a random anthem from the list.
+        If `genre` is supplied, it will pick from videos attributed with that genre.
+        If none can be found, it will log this as well as provide that information to the user.
+        """
+        if not genre:
+            return random.choice(VIDEOS)
+        else:
+            songs = [song for song in VIDEOS if genre.casefold() in song["genre"]]
+            try:
+                return random.choice(songs)
+            except IndexError:
+                pass
+
+    @commands.command(name="prideanthem", aliases=("anthem", "pridesong"))
+    async def prideanthem(self, ctx: Context, genre: str = None) -> None:
+        """
+        Sends a message with a video of a random pride anthem.
+        If `genre` is supplied, it will select from that genre only.
+        """
+        anthem = self.get_video(genre)
+        if anthem:
+            await ctx.send(anthem["url"])
+        else:
+            await ctx.send("I couldn't find a video, sorry!")
+    
+    def invalid_embed_generate(self, pride_leader: str) -> discord.Embed:
+        """
+        Generates Invalid Embed.
+        The invalid embed contains a list of closely matched names of the invalid pride
+        leader the user gave. If no closely matched names are found it would list all
+        the available pride leader names.
+        Wikipedia is a useful place to learn about pride leaders and we don't have all
+        the pride leaders, so the bot would add a field containing the wikipedia
+        command to execute.
+        """
+        embed = discord.Embed(
+            color=Colours.soft_red
+        )
+        valid_names = []
+        pride_leader = pride_leader.title()
+        for name in PRIDE_RESOURCE:
+            if fuzz.ratio(pride_leader, name) >= MINIMUM_FUZZ_RATIO:
+                valid_names.append(name)
+
+        if not valid_names:
+            valid_names = ", ".join(PRIDE_RESOURCE)
+            error_msg = "Sorry your input didn't match any stored names, here is a list of available names:"
+        else:
+            valid_names = "\n".join(valid_names)
+            error_msg = "Did you mean?"
+
+        embed.description = f"{error_msg}\n```\n{valid_names}\n```"
+        embed.set_footer(text="To add more pride leaders, feel free to open a pull request!")
+
+        return embed
+
+    def embed_builder(self, pride_leader: dict) -> discord.Embed:
+        """Generate an Embed with information about a pride leader."""
+        name = [name for name, info in PRIDE_RESOURCE.items() if info == pride_leader][0]
+
+        embed = discord.Embed(
+            title=name,
+            description=pride_leader["About"],
+            color=Colours.blue
+        )
+        embed.add_field(
+            name="Known for",
+            value=pride_leader["Known for"],
+            inline=False
+        )
+        embed.add_field(
+            name="D.O.B and Birth place",
+            value=pride_leader["Born"],
+            inline=False
+        )
+        embed.add_field(
+            name="Awards and honors",
+            value=pride_leader["Awards"],
+            inline=False
+        )
+        embed.add_field(
+            name="For More Information",
+            value=f"Do `$wiki {name}`",
+            inline=False
+        )
+        embed.set_thumbnail(url=pride_leader["url"])
+        return embed
+
+    @commands.command(aliases=("pl", "prideleader"))
+    async def pride_leader(self, ctx: Context, *, pride_leader_name: Optional[str]) -> None:
+        """
+        Information about a Pride Leader.
+        Returns information about the specified pride leader
+        and if there is no pride leader given, return a random pride leader.
+        """
+        if not pride_leader_name:
+            leader = random.choice(list(PRIDE_RESOURCE.values()))
+        else:
+            leader = PRIDE_RESOURCE.get(pride_leader_name.title())
+            if not leader:
+                embed = self.invalid_embed_generate(pride_leader_name)
+                await ctx.send(embed=embed)
+                return
+
+        embed = self.embed_builder(leader)
+        await ctx.send(embed=embed)

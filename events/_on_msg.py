e87cbb99c6d82861ebbee7295bf6a9ce0f61c76e
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-01-15 20:36:01 +0530

    Github

---
+++ events/_on_msg.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import re
+from typing import Any
+
+import discord
+
+from core import Parrot, Cog
+
+
+
+class CodeSnippets(Cog):
+    """
+    Cog that parses and sends code snippets to Discord.
+    Matches each message against a regex and prints the contents of all matched snippets.
+    """
+
+    def __init__(self, bot: Parrot):
+        """Initializes the cog's bot."""
+        self.bot = bot
+
+        
+
+    
+
+    @Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        """Checks if the message has a snippet link, removes the embed, then sends the snippet contents."""
+        if message.author.bot:
+            return
+
+
+
+def setup(bot: Parrot) -> None:
+    """Load the CodeSnippets cog."""
+    bot.add_cog(CodeSnippets(bot))

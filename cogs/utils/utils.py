0e6585a8e8ed996ccc61cedebd2fe12b5a6e0003
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-02-16 09:00:38 +0530

    fix

--- cogs/utils/utils.py
+++ cogs/utils/utils.py
@@ -482,13 +482,13 @@ class Utils(Cog):
 
     @tasks.loop(seconds=3)
     async def reminder_task(self):
-        async with asyncio.Lock():
+        async with self.lock:
             async for data in self.collection.find(
                 {"expires_at": {"$lte": datetime.datetime.utcnow().timestamp()}}
             ):
                 cog = self.bot.get_cog("EventCustom")
-                await cog.on_timer_complete(**data)
                 await self.collection.delete_one({"_id": data["_id"]})
+                await cog.on_timer_complete(**data)
 
     @reminder_task.before_loop
     async def before_reminder_task(self):
--- events/custom_events.py
+++ events/custom_events.py
@@ -5,15 +5,16 @@ from core import Cog, Parrot
 from utilities.database import parrot_db
 
 import discord
+import asyncio
 
 afk = parrot_db["afk"]
-
+timers = parrot_db["timers"]
 
 class EventCustom(Cog):
-    def __init__(self, bot: Parrot):
+    def __init__(self, bot: Parrot) -> None:
         self.bot = bot
 
-    async def on_timer_complete(self, **kw):
+    async def on_timer_complete(self, **kw) -> None:
         if kw.get("mod_action"):
             return await self.mod_action_parser(**kw.get("mod_action"))
 
@@ -37,13 +38,14 @@ class EventCustom(Cog):
                         embed=embed,
                     )
                 except discord.Forbidden:
+                    # bot is not having permissions to send messages
                     pass
 
         if kw.get("extra"):
             data = kw.get("extra")
             await self.extra_action_parser(data.get("name"), **data.get("main"))
 
-    async def mod_action_parser(self, **kw):
+    async def mod_action_parser(self, **kw) -> None:
         action: str = kw.get("action")
         guild = self.bot.get_guild(kw.get("guild"))
         if guild is None:
@@ -54,14 +56,28 @@ class EventCustom(Cog):
             try:
                 await guild.unban(discord.Object(target), reason=kw.get("reason"))
             except (discord.NotFound, discord.HTTPError, discord.Forbidden):
+                # User not found, Bot Not having permissions, Other HTTP Error
+                pass
+        
+        if action.upper() == "BAN":
+            try:
+                await guild.ban(discord.Object(target), reason=kw.get("reason"))
+            except (discord.NotFound, discord.HTTPError, discord.Forbidden):
+                # User not found, Bot Not having permissions, Other HTTP Error
                 pass
 
-    async def extra_action_parser(self, name, **kw):
+
+    async def extra_action_parser(self, name, **kw) -> None:
+        await asyncio.sleep(1)
+        # incase the parser have the excellent connection to DB
+        # and the ``delete_one`` takes time
         if name.upper() == "REMOVE_AFK":
             await afk.delete_one(kw)
         if name.upper() == "SET_AFK":
             await afk.insert_one(kw)
+        if name.upper() == "SET_TIMER":
+            await timers.insert_one(kw)
 
 
-def setup(bot: Parrot):
+def setup(bot: Parrot) -> None:
     bot.add_cog(EventCustom(bot))

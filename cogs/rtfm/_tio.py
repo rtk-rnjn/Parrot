b6f81157fffc0742535faa363fc952b08c53e864
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-02-27 23:08:51 +0530

    ?

--- cogs/rtfm/_tio.py
+++ cogs/rtfm/_tio.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from typing import Iterable
 
 import zlib
 from functools import partial
@@ -13,7 +12,7 @@ def _to_tio_string(couple):
     name, obj = couple[0], couple[1]
     if not obj:
         return b""
-    if isinstance(obj, Iterable):
+    if isinstance(obj, list):
         content = ["V" + name, str(len(obj))] + obj
         return to_bytes("\x00".join(content) + "\x00")
     return to_bytes(f"F{name}\x00{len(to_bytes(obj))}\x00{obj}\x00")

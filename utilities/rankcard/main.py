2337dc255f9a37cb7c730f88f5ed7dfc20d4e82c
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-03-05 08:59:26 +0530

    update

--- utilities/rankcard/main.py
+++ utilities/rankcard/main.py
@@ -17,12 +17,10 @@ class Rankcard:
     def rank_card(
         self, level: int, rank: int, *, current_xp: int, custom_background: str, xp_color: str, next_level_xp: int
     ) -> str:
-        
         # create backdrop
         img = Image.new('RGB', (934, 282), color = custom_background) 
         response = requests.get(self.member.display_avatar.url) # get avatar picture
         img_avatar = Image.open(BytesIO(response.content)).convert("RGBA")
-
         # create circle mask
         bigsize = (img_avatar.size[0] * 3, img_avatar.size[1] * 3)
         mask = Image.new('L', bigsize, 0)
@@ -30,7 +28,6 @@ class Rankcard:
         draw.ellipse((0, 0) + bigsize, fill=255)
         mask = mask.resize(img_avatar.size)
         img_avatar.putalpha(mask)
-
         img_avatar = img_avatar.resize((170, 170))
 
         img.paste(img_avatar, (50, 50))
@@ -41,12 +38,10 @@ class Rankcard:
         font = ImageFont.truetype(font=r"extra/fonts/Montserrat-Regular.ttf", size=50)
         font2 = ImageFont.truetype(font=r"extra/fonts/Montserrat-Regular.ttf", size=25)
 
-        # add text
         d.text((260, 100), str(self.member), (255, 255, 255), font=font)
         d.text((740, 130), f"{current_xp}/{next_level_xp} XP", (255, 255, 255), font=font2)
         d.text((650, 50), f"LEVEL {level}", xp_color, font=font)
         d.text((260, 50), f"RANK #{rank}", (255,255,255), font=font2)
-
 
         # save image
         loc = f"temp/{self.member.id}.jpg"

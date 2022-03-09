41520a61fbad3d958a14afce8ac8bafa6a362c10
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-03-09 10:56:35 +0530

    config

--- cogs/config/__init__.py
+++ cogs/config/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from .config import BotConfig
+from .config import Config
 from core import Parrot
 
 
 def setup(bot: Parrot):
-    bot.add_cog(BotConfig(bot))
+    bot.add_cog(Config(bot))

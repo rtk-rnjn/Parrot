80ed23bead139477b8265c57ece6276151b105a9
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-03-12 13:05:32 +0530

    fix

--- core/Parrot.py
+++ core/Parrot.py
@@ -234,9 +234,10 @@ class Parrot(commands.AutoShardedBot):
         VCS = await self.mongo.parrot_db.server_config.distinct("vc")
         for channel in VCS:
             if channel:
-                channel = await self.getch(self.get_channel, self.fetch_channel, channel)
+                channel = await self.getch(self.get_channel, self.fetch_channel, channel, force_fetch=False)
                 try:
-                    await channel.connect()
+                    if channel:
+                        await channel.connect()
                 except discord.ClientException:
                     pass
 

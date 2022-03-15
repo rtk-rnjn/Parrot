50436c480d619b4c22f36f6cf180a297aaa389c6
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-03-15 09:56:08 +0530

    removed topggpy

--- core/Parrot.py
+++ core/Parrot.py
@@ -86,12 +86,12 @@ class Parrot(commands.AutoShardedBot):
         self._change_log = None
         self._error_log_token = os.environ["CHANNEL_TOKEN1"]
         self.color = 0x87CEEB
-        self.topggpy = topgg.DBLClient(
-            self, dbl_token, autopost=True, post_shard_count=False
-        )
-        self.topgg_webhook = topgg.WebhookManager(self).dbl_webhook(
-            "/dblwebhook", f"{os.environ['TOPGG_AUTH']}"
-        )
+        # self.topggpy = topgg.DBLClient(
+        #     self, dbl_token, autopost=True, post_shard_count=False
+        # )
+        # self.topgg_webhook = topgg.WebhookManager(self).dbl_webhook(
+        #     "/dblwebhook", f"{os.environ['TOPGG_AUTH']}"
+        # )
         self.error_channel = None
         self.persistent_views_added = False
         self.spam_control = commands.CooldownMapping.from_cooldown(

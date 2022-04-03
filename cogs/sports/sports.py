c0afbee7c2f3035d5673bb020f43ae97a11bd386
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-04-03 23:05:14 +0530

    Fixed few bugs, and improvements

--- cogs/sports/sports.py
+++ cogs/sports/sports.py
@@ -18,59 +18,63 @@ class Sports(Cog):
         # to my localhost to be honest.
         self.url = None
 
-    @commands.group(name="ipl")
+    @commands.group(name="ipl", invoke_without_command=True)
     async def ipl(self, ctx: Context) -> None:
         """To get the IPL score"""
-        if not self.url:
-            return await ctx.send("No IPL score page set.")
+        if ctx.invoked_subcommand is None:
+            if not self.url:
+                return await ctx.send("No IPL score page set.")
 
-        url = f"http://127.0.0.1:1729/cricket_api?url={self.url}"
-        response = await self.bot.http_session.get(url)
+            url = f"http://127.0.0.1:1729/cricket_api?url={self.url}"
+            response = await self.bot.http_session.get(url)
 
-        if response.status != 200:
-            return await ctx.send("Could not get IPL score.")
+            if response.status != 200:
+                return await ctx.send("Could not get IPL score.")
 
-        data = await response.json()
+            data = await response.json()
 
-        embed = discord.Embed(
-            title=data["title"], timestamp=ctx.message.created_at
-        )
-        embed.set_footer(text=data["status"])
+            embed = discord.Embed(
+                title=data["title"], timestamp=ctx.message.created_at
+            )
+            embed.set_footer(text=data["status"])
 
-        table1 = tabulate(data['batting'], headers="keys")
-        table2 = tabulate(data['bowling'], headers="keys")
-        crr = "\n".join(data["crr"])
+            table1 = tabulate(data['batting'], headers="keys")
+            table2 = tabulate(data['bowling'], headers="keys")
+            crr = "\n".join(data["crr"])
 
-        extra = ""
-        if extra_ := data.get('extra'):
-            for temp in extra_:
-                extra += "".join(temp) + "\n"
-        embed.description = f"""
+            extra = ""
+            if extra_ := data.get('extra'):
+                for temp in extra_:
+                    extra += "".join(temp) + "\n"
+            embed.description = f"""
 > `{data['team_one']} | {data['team_two']}`
 {'`'*3}
 {crr}
 {'`'*3}
 {extra}
 """
-        if data.get('batting'):
-            embed.add_field(
-                name="Batting - Stats", value=f"```\n{table1}```", inline=False
-            )
+            if data.get('batting'):
+                embed.add_field(
+                    name="Batting - Stats", value=f"```\n{table1}```", inline=False
+                )
 
-        if data.get('bowling'):
-            embed.add_field(
-                name="Bowling - Stats", value=f"```\n{table2}```", inline=False
-            )
+            if data.get('bowling'):
+                embed.add_field(
+                    name="Bowling - Stats", value=f"```\n{table2}```", inline=False
+                )
 
-        embed.add_field(
-            name="Recent Commentry", value="- " + "\n - ".join(i for i in data["commentry"][:2] if i), inline=False
-        )
-        await ctx.send(embed=embed)
+            embed.add_field(
+                name="Recent Commentry", value="- " + "\n - ".join(i for i in data["commentry"][:2] if i), inline=False
+            )
+            await ctx.send(embed=embed)
 
 
     @with_role(*STAFF_ROLES)
     @ipl.command(name="set")
     async def set_ipl_url(self, ctx: Context, *, url: str) -> None:
         """Set the IPL score page url"""
+        if url.startswith(('<', '[')) and url.endswith(('>', ']')):
+            url = url[1:-1]
+
         self.url = url
-        await ctx.send(f"Set IPL score page to {url}")
+        await ctx.send(f"Set IPL score page to <{url}>")

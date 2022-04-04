104c25e6ab854a8f50f13fd54bb66706cbb806fb
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-04-04 13:11:08 +0530

    duplicate cmd

--- cogs/config/config.py
+++ cogs/config/config.py
@@ -288,19 +288,6 @@ class Configuration(Cog):
         if ctx.guild.me.voice:
             await ctx.guild.me.edit(voice_channel=None)
 
-    @commands.command(hidden=True)
-    @commands.has_permissions(administrator=True)
-    async def disconnect(
-        self,
-        ctx: Context,
-    ):
-        """To disconnect from the channel"""
-        if ctx.guild.me.voice:
-            await ctx.guild.me.edit(voice_channel=None)
-        await ctx.send(
-            f"{ctx.author.mention} bot will reconnect after few hours, consider removing the vc channel if permanently removing the channel"
-        )
-
     @config.command()
     @commands.has_permissions(administrator=True)
     @Context.with_type

01cfc1267c902fb343c2c3a7c2611641ec4a07eb
Ritik Ranjan <ritik0ranjan@gmail.com>
2022-04-14 19:57:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from utilities.config import TOKEN, my_secret
 bot = Parrot()
 
 
-async def main():
+async def main() -> None:
     async with ClientSession(
         connector=TCPConnector(resolver=AsyncResolver(), family=socket.AF_INET)
     ) as http_session:

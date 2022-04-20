0d189fb1138e5ea014381ef1944fce1f861e936d
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-04-21 02:33:46 +0530

    swap

--- events/on_msg.py
+++ events/on_msg.py
@@ -926,8 +926,8 @@ class OnMsg(Cog, command_attrs=dict(hidden=True)):
     @tasks.loop(seconds=10)
     async def _10s_task(self):
         temp = self.write_data
-        self.write_data.clear()
         await self.bot.mongo.msg_db.content.bulk_write(temp)
+        self.write_data.clear()
 
 async def setup(bot: Parrot) -> None:
     await bot.add_cog(OnMsg(bot))

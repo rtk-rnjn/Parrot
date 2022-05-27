0a80f13b91ba9b5cf05a8e7104ef6e1b501f20bd
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-05-28 04:22:47 +0530

    ..

--- interactions/slash/__init__.py
+++ interactions/slash/__init__.py
@@ -15,7 +15,7 @@ class ContextMenu(Cog):
         self.ctx_menu = app_commands.ContextMenu(
             name="Interpret as command",
             # description="Interpret the message as a command.",
-            guild_ids=[978694756022489098],
+            guild_ids=[guild.id for guild in self.bot.guilds],
             callback=self.ctx_menu
         )
         self.bot.tree.add_command(self.ctx_menu)
@@ -24,7 +24,7 @@ class ContextMenu(Cog):
         self.bot.tree.remove_command(self.ctx_menu)
 
     async def ctx_menu(self, interaction: discord.Interaction, message: discord.Message) -> None:
-        await interaction.response.defer(thinking=True)
+        await interaction.response.defer()
         prefix = await self.bot.get_guild_prefixes(message.guild)
         if prefix is not None and not message.content.startswith(prefix):
             message.content = f"{prefix}{message.content}"

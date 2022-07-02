431a73e682319de12092995d565f2b0a903df681
Ritik Ranjan <ritik0ranjan@gmail.com>
2022-07-02 08:55:15 +0000

    Add country guess

---
+++ interactions/buttons/__country_guess.py
@@ -0,0 +1,363 @@
+from __future__ import annotations
+import asyncio
+import difflib
+from io import BytesIO
+import os
+import pathlib
+import random
+
+from typing import Final, Optional, TypeAlias, Union
+
+import discord
+from discord.ext import commands
+
+from utilities.converters import ToAsync
+from PIL import Image, ImageFilter, ImageOps
+
+DiscordColor: TypeAlias = Union[discord.Color, int]
+DEFAULT_COLOR: Final[discord.Color] = discord.Color(0x2F3136)
+
+class CountryGuesser:
+    """
+    CountryGuesser Game
+    """
+    embed: discord.Embed
+    accepted_length: Optional[int]
+    country: str
+
+    def __init__(
+        self,
+        *,
+        is_flags: bool = False,
+        light_mode: bool = False,
+        hard_mode: bool = False,
+        guesses: int = 5,
+        hints: int = 1
+    ) -> None:
+
+        self.embed_color: Optional[DiscordColor] = None
+        self.hints = hints
+        self.guesses = guesses
+
+        self.is_flags = is_flags
+        self.hard_mode = hard_mode
+
+        if self.is_flags:
+            self.light_mode: bool = False
+        else:
+            self.light_mode: bool = light_mode
+
+        folder = 'assets/country-flags' if self.is_flags else 'assets/country-data'
+        self._countries_path = pathlib.Path(__file__).parent / folder
+
+        self.all_countries = os.listdir(self._countries_path)
+
+    @ToAsync()
+    def invert_image(self, image_path: Union[BytesIO, os.PathLike, str]) -> BytesIO:
+        with Image.open(image_path) as img:
+            img = img.convert('RGBA')
+            r, g, b, a = img.split()
+            rgb = Image.merge('RGB', (r, g, b))
+            rgb = ImageOps.invert(rgb)
+            rgb = rgb.split()
+            img = Image.merge('RGBA', rgb + (a,))
+
+            buf = BytesIO()
+            img.save(buf, 'PNG')
+            buf.seek(0)
+            return buf
+
+    @ToAsync()
+    def blur_image(self, image_path: Union[BytesIO, os.PathLike, str]) -> BytesIO:
+        with Image.open(image_path) as img:
+            img = img.convert('RGBA')
+            img = img.filter(ImageFilter.GaussianBlur(10))
+
+            buf = BytesIO()
+            img.save(buf, 'PNG')
+            buf.seek(0)
+            return buf
+
+    async def get_country(self) -> discord.File:
+        country_file = random.choice(self.all_countries)
+        self.country = country_file.strip()[:-4].lower()
+
+        file = os.path.join(self._countries_path, country_file)
+
+        if self.hard_mode:
+            file = await self.blur_image(file)
+
+        if self.light_mode:
+            file = await self.invert_image(file)
+
+        return discord.File(file, 'country.png')
+
+    def get_blanks(self) -> str:
+        return ' '.join('_' if char != ' ' else ' ' for char in self.country)
+
+    def get_hint(self) -> str:
+        blanks = ['_' if char != ' ' else ' ' for char in self.country]
+        times = round(len(blanks) / 3)
+
+        for _ in range(times):
+            idx = random.choice(range(len(self.country)))
+            blanks[idx] = self.country[idx]
+        return ' '.join(blanks)
+
+    def get_accuracy(self, guess: str) -> int:
+        return round(difflib.SequenceMatcher(None, guess, self.country).ratio() * 100)
+
+    def get_embed(self) -> discord.Embed:
+        embed = discord.Embed(
+            title='Guess that country!',
+            description=f'```fix\n{self.get_blanks()}\n```',
+            color=self.embed_color,
+        )
+        embed.add_field(
+            name='\u200b',
+            value=f'```yml\nblurred: {str(self.hard_mode).lower()}\nflag-mode: {str(self.is_flags).lower()}\n```',
+            inline=False,
+        )
+        embed.set_image(url='attachment://country.png')
+        return embed
+
+    async def wait_for_response(
+        self,
+        ctx: commands.Context[commands.Bot],
+        *,
+        options: tuple[str, ...] = (),
+        length: Optional[int] = None,
+    ) -> Optional[tuple[discord.Message, str]]:
+
+        def check(m: discord.Message) -> bool:
+            if length:
+                return m.channel == ctx.channel and m.author == ctx.author and len(m.content) == length
+            else:
+                return m.channel == ctx.channel and m.author == ctx.author
+
+        message: discord.Message = await ctx.bot.wait_for('message', timeout=self.timeout, check=check)
+        content = message.content.strip().lower()
+
+        if options:
+            if not content in options:
+                return
+
+        return message, content
+
+    async def start(
+        self,
+        ctx: commands.Context[commands.Bot],
+        *,
+        timeout: Optional[float] = None,
+        embed_color: DiscordColor = DEFAULT_COLOR,
+        ignore_diff_len: bool = False
+    ) -> discord.Message:
+        """
+        starts the country-guesser game
+        Parameters
+        ----------
+        ctx : commands.Context
+            the context of the invokation command
+        timeout : Optional[float], optional
+            the timeout for when waiting, by default None
+        embed_color : DiscordColor, optional
+            the color of the game embed, by default DEFAULT_COLOR
+        ignore_diff_len : bool, optional
+            specifies whether or not to ignore guesses that are not of the same length as the correct answer, by default False
+        Returns
+        -------
+        discord.Message
+            returns the game message
+        """
+        file = await self.get_country()
+
+        self.timeout = timeout
+        self.embed_color = embed_color
+        self.embed = self.get_embed()
+        self.embed.set_footer(text='send your guess into the chat now!')
+
+        self.message = await ctx.send(embed=self.embed, file=file)
+
+        self.accepted_length = len(self.country) if ignore_diff_len else None
+
+        while not ctx.bot.is_closed():
+            try:
+                msg, response = await self.wait_for_response(ctx, length=self.accepted_length)
+            except asyncio.TimeoutError:
+                break
+
+            if response == self.country:
+                await msg.reply(f'That is correct! The country was `{self.country.title()}`')
+                break
+            else:
+                self.guesses -= 1
+
+                if not self.guesses:
+                    await msg.reply(f'Game Over! you lost, The country was `{self.country.title()}`')
+                    break
+
+                acc = self.get_accuracy(response)
+
+                if not self.hints:
+                    await msg.reply(f'That was incorrect! but you are `{acc}%` of the way there!\nYou have **{self.guesses}** guesses left.', mention_author=False)
+                else:
+                    await msg.reply(f'That is incorrect! but you are `{acc}%` of the way there!\nWould you like a hint? type: `(y/n)`', mention_author=False)
+
+                    try:
+                        hint_msg, resp = await self.wait_for_response(ctx, options=('y', 'n'))
+                    except asyncio.TimeoutError:
+                        break
+                    else:
+                        if resp == 'y':
+                            hint = self.get_hint()
+                            self.hints -= 1
+                            await hint_msg.reply(f'Here is your hint: `{hint}`', mention_author=False)
+                        else:
+                            await hint_msg.reply(f'Okay continue guessing! You have **{self.guesses}** guesses left.', mention_author=False)
+
+        return self.message
+
+class CountryInput(discord.ui.Modal, title='Input your guess!'):
+    
+    def __init__(self, view: CountryView) -> None:
+        super().__init__()
+        self.view = view
+
+        self.guess = discord.ui.TextInput(
+            label='Input your guess',
+            style=discord.TextStyle.short,
+            required=True,
+            max_length=self.view.game.accepted_length,
+        )
+
+        self.add_item(self.guess)
+        
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        guess = self.guess.value.strip().lower()
+        game = self.view.game
+
+        if guess == game.country:
+            game.update_guesslog('+ GAME OVER, you won! +')
+            await interaction.response.send_message(f'That is correct! The country was `{game.country.title()}`')
+
+            self.view.disable_all()
+            game.embed.description = f'```fix\n{game.country.title()}\n```'
+            await interaction.message.edit(view=self.view, embed=game.embed)
+            return self.view.stop()
+        else:
+            game.guesses -= 1
+
+            if not game.guesses:
+                self.view.disable_all()
+                game.update_guesslog('- GAME OVER, you lost -')
+
+                await interaction.message.edit(embed=game.embed, view=self.view)
+                await interaction.response.send_message(f'Game Over! you lost, The country was `{game.country.title()}`')
+                return self.view.stop()
+            else:
+                acc = game.get_accuracy(guess)
+                game.update_guesslog(
+                    f'- [{guess}] was incorrect! but you are ({acc}%) of the way there!\n'
+                    f'+ You have {game.guesses} guesses left.\n'
+                )
+
+                await interaction.response.edit_message(embed=game.embed)
+
+
+class CountryView(discord.ui.View):
+    
+    def __init__(self, game: BetaCountryGuesser, *, user: discord.User, timeout: float) -> None:
+        super().__init__(timeout=timeout)
+
+        self.game = game
+        self.user = user
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if interaction.user != self.user:
+            await interaction.response.send_message(f'This is not your game!', ephemeral=True)
+            return False
+        else:
+            return True
+
+    def disable_all(self) -> None:
+        for button in self.children:
+            if isinstance(button, discord.ui.Button):
+                button.disabled = True
+
+    async def on_timeout(self) -> None:
+        return self.stop()
+
+    @discord.ui.button(label='Make a guess!', style=discord.ButtonStyle.blurple)
+    async def guess_button(self, interaction: discord.Interaction, _) -> None:
+        return await interaction.response.send_modal(CountryInput(self))
+
+    @discord.ui.button(label='hint', style=discord.ButtonStyle.green)
+    async def hint_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        hint = self.game.get_hint()
+        self.game.hints -= 1
+        await interaction.response.send_message(f'Here is your hint: `{hint}`', ephemeral=True)
+
+        if not self.game.hints:
+            button.disabled = True
+            await interaction.message.edit(view=self)
+            
+    @discord.ui.button(label='Cancel', style=discord.ButtonStyle.red)
+    async def cancel_button(self, interaction: discord.Interaction, _) -> None:
+        self.disable_all()
+
+        self.game.embed.description = f'```fix\n{self.game.country.title()}\n```'
+        self.game.update_guesslog('- GAME OVER, CANCELLED -')
+
+        await interaction.response.send_message(f'Game Over! The country was `{self.game.country.title()}`')
+        await interaction.message.edit(view=self, embed=self.game.embed)
+        return self.stop()
+
+
+class BetaCountryGuesser(CountryGuesser):
+    """
+    Country Guesser(buttons) Game
+    """
+    guesslog: str = ''
+
+    def update_guesslog(self, entry: str) -> None:
+        self.guesslog += entry + '\n'
+        self.embed.set_field_at(1, name='Guess Log', value=f'```diff\n{self.guesslog}\n```')
+
+    async def start(
+        self, 
+        ctx: commands.Context[commands.Bot], 
+        *, 
+        embed_color: DiscordColor = DEFAULT_COLOR,
+        ignore_diff_len: bool = False,
+        timeout: Optional[float] = None,
+    ) -> discord.Message:  
+        """
+        starts the Country Guesser(buttons) Game
+        Parameters
+        ----------
+        ctx : commands.Context
+            the context of the invokation command
+        embed_color : DiscordColor, optional
+            the color of the game embed, by default DEFAULT_COLOR
+        ignore_diff_len : bool, optional
+            specifies whether or not to ignore guesses that are not of the same length as the correct answer, by default False
+        timeout : Optional[float], optional
+            the timeout for the view, by default None
+        Returns
+        -------
+        discord.Message
+            returns the game message
+        """
+        self.accepted_length = len(self.country) if ignore_diff_len else None
+
+        file = await self.get_country()
+
+        self.embed_color = embed_color
+        self.embed = self.get_embed()
+        self.embed.add_field(name='Guess Log', value='```diff\n\u200b\n```', inline=False)
+
+        self.view = CountryView(self, user=ctx.author, timeout=timeout)
+        self.message = await ctx.send(embed=self.embed, file=file, view=self.view)
+
+        await self.view.wait()
+        return self.message
---
+++ interactions/buttons/__light_out.py
@@ -0,0 +1,226 @@
+from __future__ import annotations
+import asyncio
+
+import random
+from typing import Any, TYPE_CHECKING, Coroutine, Optional, Literal, Final, TypeVar, Union
+
+import discord
+from discord.ext import commands
+
+from .__number_slider import SlideView, DEFAULT_COLOR, DiscordColor
+
+
+if TYPE_CHECKING:
+    from typing_extensions import TypeAlias
+    Board: TypeAlias = list[list[Optional[Literal['\N{ELECTRIC LIGHT BULB}']]]]
+
+BULB: Final[Literal['\N{ELECTRIC LIGHT BULB}']] = '\N{ELECTRIC LIGHT BULB}'
+
+
+if TYPE_CHECKING:
+    from typing_extensions import ParamSpec, TypeAlias
+
+    P = ParamSpec('P')
+    T = TypeVar('T')
+
+    A = TypeVar('A', bool)
+    B = TypeVar('B', bool)
+
+
+def chunk(iterable: list[int], *, count: int) -> list[list[int]]:
+    return [iterable[i:i + count] for i in range(0, len(iterable), count)]
+
+
+async def wait_for_delete(
+    ctx: commands.Context[commands.Bot],
+    message: discord.Message,
+    *,
+    emoji: str = '\N{BLACK SQUARE FOR STOP}',
+    bot: Optional[discord.Client] = None,
+    user: Optional[Union[discord.User, tuple[discord.User, ...]]] = None,
+    timeout: Optional[float] = None,
+) -> bool:
+
+    if not user:
+        user = ctx.author
+    try:
+        await message.add_reaction(emoji)
+    except discord.DiscordException:
+        pass
+
+    def check(reaction: discord.Reaction, _user: discord.User) -> bool:
+        if reaction.emoji == emoji and reaction.message == message:
+            if isinstance(user, tuple):
+                return _user in user
+            else:
+                return _user == user
+
+    bot: discord.Client = bot or ctx.bot
+    try:
+        await bot.wait_for('reaction_add', timeout=timeout, check=check)
+    except asyncio.TimeoutError:
+        return False
+    else:
+        await message.delete()
+        return True
+
+
+async def double_wait(
+    task1: Coroutine[Any, Any, A],
+    task2: Coroutine[Any, Any, B],
+    *,
+    loop: Optional[asyncio.AbstractEventLoop] = None,
+) -> tuple[
+        set[asyncio.Task[Union[A, B]]],
+        set[asyncio.Task[Union[A, B]]],
+    ]:
+
+    if not loop:
+        loop = asyncio.get_event_loop()
+
+    return await asyncio.wait(
+        [
+            loop.create_task(task1),
+            loop.create_task(task2),
+        ],
+        return_when=asyncio.FIRST_COMPLETED,
+    )
+
+
+class LightsOutButton(discord.ui.Button['LightsOutView']):
+
+    def __init__(self, emoji: str, *, style: discord.ButtonStyle, row: int, col: int) -> None:
+        super().__init__(
+            emoji=emoji,
+            label='\u200b',
+            style=style,
+            row=row,
+        )
+
+        self.col = col
+
+    async def callback(self, interaction: discord.Interaction) -> None:
+        game = self.view.game
+
+        if interaction.user != game.player:
+            return await interaction.response.send_message('This is not your game!', ephemeral=True)
+        else:
+            row, col = self.row, self.col
+
+            beside_item = game.beside_item(row, col)
+            game.toggle(row, col)
+
+            for i, j in beside_item:
+                game.toggle(i, j)
+
+            self.view.update_board(clear=True)
+
+            game.moves += 1
+            game.embed.set_field_at(0, name='\u200b', value=f'Moves: `{game.moves}`')
+
+            if game.tiles == game.completed:
+                self.view.disable_all()
+                self.view.stop()
+                game.embed.description = '**Congrats! You won!**'
+                    
+            return await interaction.response.edit_message(embed=game.embed, view=self.view)
+
+
+class LightsOutView(SlideView):
+    game: LightsOut
+
+    def __init__(self, game: LightsOut, *, timeout: float) -> None:
+        super().__init__(game, timeout=timeout)
+
+    def update_board(self, *, clear: bool = False) -> None:
+
+        if clear:
+            self.clear_items()
+            
+        for i, row in enumerate(self.game.tiles):
+            for j, tile in enumerate(row):
+                button = LightsOutButton(
+                    emoji=tile,
+                    style=self.game.button_style,
+                    row=i,
+                    col=j,
+                )
+                self.add_item(button)
+
+
+class LightsOut:
+    """
+    Lights Out Game
+    """
+    def __init__(self, count: Literal[1, 2, 3, 4, 5] = 4) -> None:
+
+        if count not in range(1, 6):
+            raise ValueError('Count must be an integer between 1 and 5')
+
+        self.moves: int = 0
+        self.count = count
+
+        self.completed: Final[Board] = [[None] * self.count for _ in range(self.count)]
+        self.tiles: Board = []
+
+        self.player: Optional[discord.User] = None
+        self.button_style: discord.ButtonStyle = discord.ButtonStyle.green
+
+    def toggle(self, row: int, col: int) -> None:
+        self.tiles[row][col] = BULB if self.tiles[row][col] is None else None
+
+    def beside_item(self, row: int, col: int) -> list[tuple[int, int]]:
+        beside = [
+            (row - 1, col), 
+            (row, col - 1), 
+            (row + 1, col), 
+            (row, col + 1),
+        ]
+
+        data = [
+            (i, j) for i, j in beside if i in range(self.count) and j in range(self.count)
+        ]
+        return data
+
+    async def start(
+        self, 
+        ctx: commands.Context[commands.Bot],
+        *,
+        button_style: discord.ButtonStyle = discord.ButtonStyle.green,
+        embed_color: DiscordColor = DEFAULT_COLOR,
+        timeout: Optional[float] = None
+    ) -> discord.Message:
+        """
+        starts the Lights Out Game
+        Parameters
+        ----------
+        ctx : commands.Context
+            the context of the invokation command
+        button_style : discord.ButtonStyle, optional
+            the primary button style to use, by default discord.ButtonStyle.green
+        embed_color : DiscordColor, optional
+            the color of the game embed, by default DEFAULT_COLOR
+        timeout : Optional[float], optional
+            the timeout for the view, by default None
+        Returns
+        -------
+        discord.Message
+            returns the game message
+        """
+        self.button_style = button_style
+        self.player = ctx.author
+
+        self.tiles = random.choices((None, BULB), k=self.count ** 2)
+        self.tiles = chunk(self.tiles, count=self.count)
+
+        self.view = LightsOutView(self, timeout=timeout)
+        self.embed = discord.Embed(description='Turn off all the tiles!', color=embed_color)
+        self.embed.add_field(name='\u200b', value='Moves: `0`')
+
+        self.message = await ctx.send(embed=self.embed, view=self.view)
+
+        await double_wait(
+            wait_for_delete(ctx, self.message), 
+            self.view.wait(),
+        )
+        return self.message

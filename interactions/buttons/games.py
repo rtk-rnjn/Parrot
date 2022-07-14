0af3b2a39b935e4d391dc860693fd998725f0472
Ritik Ranjan <ritik0ranjan@gmail.com>
2022-07-14 10:25:12 +0000

    improved cmd quality

--- interactions/buttons/games.py
+++ interactions/buttons/games.py
@@ -2370,48 +2370,32 @@ class Games(Cog):
 
         FILTER = {"twenty48": {"$exists": True}}
 
-        if flag.me and flag._global:
-            return await ctx.send(
-                f"{ctx.author.mention} you can't use both `--me` and `--global` at the same time!"
-            )
-
         if flag.me:
-            data = await col.find_one(
-                {"_id": user.id, **FILTER},
+            FILTER["_id"] = user.id
+        elif not flag._global:
+            FILTER["_id"] = {"$in": [m.id for m in ctx.guild.members]}
+        entries = []
+        async for data in col.find(FILTER):
+            user = await self.bot.getch(
+                self.bot.get_user, self.bot.fetch_user, data["_id"]
             )
-            if not data:
-                await ctx.send(
-                    f"{ctx.author.mention + ' you' if user is ctx.author else user} haven't played 2048 yet!"
-                )
-                return
-            return await ctx.send(
-                f"{ctx.author.mention + ' your' if user is ctx.author else user}'s stats:\n"
-                f"`Games Played`: {data['twenty48']['games_played']}\n"
-                f"`Total Moves `: {data['twenty48']['total_moves']}\n"
-            )
-        if flag._global:
-            entries = []
-            async for data in col.find(FILTER):
-                user = await self.bot.getch(
-                    self.bot.get_user, self.bot.fetch_user, data["_id"]
-                )
-                entries.append(
-                    f"""User: `{user or 'NA'}`
+            entries.append(
+                f"""User: `{user or 'NA'}`
 `Games Played`: {data['twenty48']['games_played']} games played
 `Total Moves `: {data['twenty48']['total_moves']} moves
 """
-                )
-            if not entries:
-                await ctx.send(f"{ctx.author.mention} No one has played 2048 yet!")
-                return
+            )
+        if not entries:
+            await ctx.send(f"{ctx.author.mention} No one has played 2048 yet!")
+            return
 
-            def func(e):
-                return e[sort_by]
+        def func(e):
+            return e[sort_by]
 
-            entries.sort(reverse=bool(flag.sort), key=func)
+        entries.sort(reverse=bool(flag.sort), key=func)
 
-            p = SimplePages(entries, ctx=ctx)
-            await p.start()
+        p = SimplePages(entries, ctx=ctx)
+        await p.start()
 
     @top.command(name="countryguess")
     async def country_guess_stats(
@@ -2445,21 +2429,8 @@ class Games(Cog):
             )
 
         if flag.me:
-            data = await col.find_one(
-                {"_id": user.id, **FILTER},
-            )
-            if not data:
-                await ctx.send(
-                    f"{ctx.author.mention + ' you' if user is ctx.author else user} haven't played Country Guess yet!"
-                )
-                return
-            return await ctx.send(
-                f"{ctx.author.mention + ' your' if user is ctx.author else user}'s stats: \n"
-                f"`Games Played`: {data['countryguess'].get('games_played')} games played\n"
-                f"`Games Won`: {data['countryguess'].get('games_won')} games won\n"
-            )
-
-        if not flag._global:
+            FILTER["_id"] = user.id
+        elif not flag._global:
             FILTER["_id"] = {"$in": [m.id for m in ctx.guild.members]}
 
         entries = []
@@ -2474,7 +2445,7 @@ class Games(Cog):
 """
             )
         if not entries:
-            await ctx.send(f"{ctx.author.mention} No one has played Country Guess yet!")
+            await ctx.send(f"{ctx.author.mention} No records found")
             return
 
         def func(e):
@@ -2536,35 +2507,40 @@ class Games(Cog):
             if ctx.author.id in {user1.id, user2.id}:
                 entries.append(
                     f"""**{user1 or 'NA'} vs {user2 or 'NA'}**
-    `Winner`: {i["winner"]}
-    `Draw  `: {i["draw"]}
-    """
+`Winner`: {i["winner"]}
+`Draw  `: {i["draw"]}
+"""
                 )
             else:
                 entries.append(
                     f"""{user1 or 'NA'} vs {user2 or 'NA'}
-    `Winner`: {i["winner"]}
-    `Draw  `: {i["draw"]}
-    """
+`Winner`: {i["winner"]}
+`Draw  `: {i["draw"]}
+"""
                 )
         p = SimplePages(entries, ctx=ctx)
         await p.start()
 
     @top.command(name="reaction")
     async def top_reaction(self, ctx: Context, *, flag: ReactionStatsFlag):
-        """Reaction Test Stats"""
+        """Reaction Test Stats
+
+        Flag Options:
+        `--me`: Only show your stats
+        `--global`: Show global stats
+        `--sort`: Sort the list in ascending or descending order. `-1` (decending) or `1` (ascending)
+        `--limit`: Limit the list to the top `limit` entries.
+        """
         entries = []
         i = 1
-        FILTER = {}
+        FILTER = {"reaction_test": {"$exists": True}}
         if flag.me:
-            FILTER = {"_id": ctx.author.id}
-        if not flag._global:
+            FILTER["_id"] = ctx.author.id
+        elif not flag._global:
             FILTER["_id"] = {"$in": [m.id for m in ctx.guild.members]}
 
         col: Collection = self.bot.mongo.extra.games_leaderboard
-        async for data in col.find(FILTER, {"reaction_test": {"$exists": True}}).sort(
-            "reaction_test", flag.sort
-        ):
+        async for data in col.find(FILTER).sort("reaction_test", flag.sort):
             user = await self.bot.getch(
                 self.bot.get_user, self.bot.fetch_user, data["_id"]
             )
@@ -2596,24 +2572,24 @@ class Games(Cog):
         """Typing Test Stats
 
         Flag Options:
+        `--me`: Only show your stats
+        `--global`: Show global stats
         `--sort_by`: Sort the list by any of the following: `speed`, `accuracy`, `wpm`
         `--sort`: Sort the list in ascending or descending order. `-1` (decending) or `1` (ascending)
         `--limit`: Limit the list to the top `limit` entries.
         """
         entries = []
         i = 1
-        FILTER = {}
+        FILTER = {"typing_test": {"$exists": True}}
         if flag.me:
             FILTER["_id"] = ctx.author.id
-
-        if not flag._global:
+        elif not flag._global:
             FILTER = {"_id": {"$in": [m.id for m in ctx.guild.members]}}
 
         col: Collection = self.bot.mongo.extra.games_leaderboard
 
         async for data in col.find(
             FILTER,
-            {"typing_test": {"$exists": True}},
         ).sort(f"typing_test.{flag.sort_by}", flag.sort):
 
             user = await self.bot.getch(
@@ -2641,9 +2617,7 @@ class Games(Cog):
             i += 1
 
         if not entries:
-            await ctx.send(
-                f"{ctx.author.mention} No records found"
-            )
+            await ctx.send(f"{ctx.author.mention} No records found")
             return
 
         p = SimplePages(entries, ctx=ctx)

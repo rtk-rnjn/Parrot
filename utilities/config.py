4e607f0786e24233425a9739533c94168a3777a8
Ritik Ranjan <ritik0ranjan@gmail.com>
2022-07-28 18:46:35 +0000

    Code formatting

--- utilities/config.py
+++ utilities/config.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import os
-from typing import Any, Dict, Union
+from typing import Any, Dict, List, Union
 
 import yaml
 
@@ -16,16 +16,18 @@ except ImportError:
 with open("config.yml") as f:
     data: Dict[str, Any] = yaml.safe_load(f.read())
 
-VERSION = "v4.8.0-beta"
+VERSION = "v4.9.5-beta"
 
 
-def parse_env_var(key: str, default: Any = None) -> Union[str, int, float, bool]:
+def parse_env_var(key: str, default: Any = None) -> Union[str, int, float, bool, List[Any]]:
     """
     Parse an environment variable into a Python type.
     """
     value = os.environ.get(key, default)
     if value is None:
         raise ValueError(f"{key} is not set")
+    if "|" in value:
+        return [parse_env_var(key) for key in value.split("|")]
     if value.isdigit():
         return int(value)
     if value.replace(".", "", 1).isdigit():
@@ -35,32 +37,23 @@ def parse_env_var(key: str, default: Any = None) -> Union[str, int, float, bool]
     return value
 
 
-OWNER_IDS: list = data["owner_ids"]
+OWNER_IDS: list = parse_env_var("OWNER_IDS")
 DEFAULT_PREFIX: str = parse_env_var("BOT_PREFIX", "$")
 CASE_INSENSITIVE: bool = parse_env_var("COMMAND_CASE_INSENSITIVE", True)
 STRIP_AFTER_PREFIX: bool = parse_env_var("STRIP_AFTER_PREFIX")
-
 SUPER_USER: str = parse_env_var("OWNER_ID")
 MASTER_OWNER: str = SUPER_USER
-
 EXTENSIONS: str = data["extensions"]
 DEV_LOGO: str = data["dev_logo"]
-
 TOKEN: str = parse_env_var("TOKEN")
 DATABASE_KEY: str = parse_env_var("DATABASE_KEY")
 OPEN_ROBOT_API: str = parse_env_var("OPEN_ROBOT_API")
 my_secret = DATABASE_KEY
-
-AUTHOR_NAME: str = data["author_name"]
+AUTHOR_NAME: str = parse_env_var("OWNER_NAME")
 AUTHOR_DISCRIMINATOR: int = parse_env_var("OWNER_DISCRIMINATOR", 9230)
-
-GITHUB: str = f"https://github.com/{parse_env_var('GITHUB_ID', 'rtk-rnjn')}/{parse_env_var('GITHUB_REPO', 'Parrot')}"
+GITHUB: str = f"https://github.com/{parse_env_var('GITHUB_ID')}/{parse_env_var('GITHUB_REPO')}"
 SUPPORT_SERVER: str = f"https://discord.gg/{parse_env_var('SUPPORT_SERVER')}"
 WEBHOOK_JOIN_LEAVE_CHANNEL_ID: int = parse_env_var("WEBHOOK_JOIN_LEAVE_ID")
 SUPPORT_SERVER_ID = parse_env_var("SUPPORT_SERVER_ID")
-
 MEME_PASS = parse_env_var("MEME_PASS")
-# this is not the real pass, just to tell the website from where the API call is comming from.
-# yes you can use. but i suggest you to use your own.
-
-PRIVACY_POLICY: str = data["privacy_policy"]
+PRIVACY_POLICY: str = parse_env_var("PRIVACY_POLICY")

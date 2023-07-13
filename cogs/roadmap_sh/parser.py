3a750de57dbf2c81732ef1e7561b3c8da8359d8a
rtk-rnjn <ritik0ranjan@gmail.com>
2023-07-13 15:09:53 +0530

    fixing parser

--- cogs/roadmap_sh/parser.py
+++ cogs/roadmap_sh/parser.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+import re
 
 import aiofiles
 from jishaku.paginators import PaginatorEmbedInterface
@@ -42,10 +43,17 @@ class ParentView(discord.ui.View):
 
     async def start(self, ctx: Context) -> None:
         self.ctx = ctx
-        message = await ctx.send
+        self.message = await ctx.send(
+            embed=discord.Embed(
+                title="Developer Roadmaps",
+                description="[roadmap.sh](https://roadmap.sh) is a community effort to create roadmaps, guides and other educational content to help guide developers in picking up a path and guide their learnings.",
+                url="https://roadmap.sh/",
+            ),
+            view=self,
+        )
 
 
-class FolderView(discord.ui.Select):
+class ContentView(discord.ui.Select):
     view: ParentView
 
     def __init__(self, *, folders: list[str]) -> None:
@@ -53,6 +61,19 @@ class FolderView(discord.ui.Select):
 
         for folder in folders:
             self.add_option(label=folder, value=folder)
+    
+    def remove_img_links(self, text: str) -> str:
+        """Remove all ![]() links from text"""
+        return re.sub(r"!\[.*\]\(.*\)", "", text)
+
+    def replace_partial_links(self, text: str) -> str:
+        def f(m: re.Match) -> str:
+            title,link = m.groups()
+            if link.startswith("/"):
+                link = f"https://roadmap.sh{link}"
+            return f"{title}({link})"
+        
+        return re.sub(r"(\[.*?\])\((.*?)\)", f, text)
 
     async def prepare_file_embed(self, path: str):
         async with aiofiles.open(path, mode="r", encoding="utf-8") as f:
@@ -66,6 +87,8 @@ class FolderView(discord.ui.Select):
                 _ADD_LINE = not _ADD_LINE
                 continue
             if _ADD_LINE:
+                line = self.remove_img_links(line)
+                line = self.replace_partial_links(line)
                 page.add_line(line)
 
         interference = PaginatorEmbedInterface(self.view.ctx, page, owner=self.view.ctx.author)
@@ -75,11 +98,12 @@ class FolderView(discord.ui.Select):
         await interaction.response.defer()
 
         folder = self.values[0]
-        # check if the `folder` is a file
-        if not os.path.isdir(folder):
-            view = FileView(files=[folder])
-
-
-class FileView(discord.ui.View):
-    def __init__(self, *, files: list[str]) -> None:
-        super().__init__(timeout=60)
+        if os.path.isdir(folder):
+            await self.view.message.edit(
+                embed=discord.Embed(
+                    title="Developer Roadmaps",
+                    description=f"**{folder}**",
+                    url="https://roadmap.sh/",
+                )
+            )
+            await self.prepare_file_embed(os.path.join(folder, "README.md"))

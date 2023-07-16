257a358b08dffbea434c0ac974bc4e6092ba839f
rtk-rnjn <ritik0ranjan@gmail.com>
2023-07-17 02:45:51 +0530

    fix typo

--- cogs/defcon/__init__.py
+++ cogs/defcon/__init__.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from core import Parrot
 
-from .defcon import Defcon
+from .defcon import DefensiveCondition
 from .events import DefconListeners
 
 
 async def setup(bot: Parrot) -> None:
-    await bot.add_cog(Defcon(bot))
+    await bot.add_cog(DefensiveCondition(bot))
     await bot.add_cog(DefconListeners(bot))

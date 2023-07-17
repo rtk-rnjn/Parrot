cbf4e00b3163ba131f83a27bf8ef2bfecb6e91c9
rtk-rnjn <ritik0ranjan@gmail.com>
2023-07-17 20:31:22 +0530

    removed slots

--- cogs/autoresponder/variables.py
+++ cogs/autoresponder/variables.py
@@ -11,35 +11,20 @@ import discord
 class Variables:
     __class__ = None  # type: ignore
 
-    __slots__ = (
-        "message_id",
-        "message_content",
-        "channel_id",
-        "channel_name",
-        "message_author_id",
-        "message_author_name",
-        "message_author",
-        "get_role_name",
-        "get_channel_name",
-        "get_member_name",
-        "create_channel",
-        "create_role",
-    )
-
     def __init__(self, *, message: discord.Message, bot: Parrot):
-        self._message = message  # type: ignore
-        self._bot = bot  # type: ignore
+        self.__message = message
+        self.__bot = bot
 
     def build_base(self):
-        self.message_id = self._message.id
-        self.message_content = self._message.content
+        self.message_id = self.__message.id
+        self.message_content = self.__message.content
 
-        self.channel_id = self._message.channel.id
-        self.channel_name = self._message.channel.name  # type: ignore
+        self.channel_id = self.__message.channel.id
+        self.channel_name = self.__message.channel.name  # type: ignore
 
-        self.message_author_id = self._message.author.id
-        self.message_author_name = self._message.author.name
-        self.message_author = str(self._message.author)
+        self.message_author_id = self.__message.author.id
+        self.message_author_name = self.__message.author.name
+        self.message_author = str(self.__message.author)
 
         payload = {
             "message_id": self.message_id,
@@ -65,20 +50,22 @@ class Variables:
         return payload
 
     def _get_role_name(self, role_id: int):
-        r = self._message.guild.get_role(role_id)  # type: ignore
+        r = self.__message.guild.get_role(role_id)  # type: ignore
         return r.name if r else None
 
     def _get_channel_name(self, channel_id: int):
-        c = self._message.guild.get_channel(channel_id)  # type: ignore
+        c = self.__message.guild.get_channel(channel_id)  # type: ignore
         return c.name if c else None
 
     def _get_member_name(self, member_id: int):
-        m = self._message.guild.get_member(member_id)  # type: ignore
+        m = self.__message.guild.get_member(member_id)  # type: ignore
         return m.name if m else None
 
     async def _create_channel(self, name: str, position: int = None, category: int = None):
-        cat = self._message.guild.get_channel(category) if category else None  # type: discord.CategoryChannel # type: ignore
-        chn = await self._message.guild.create_text_channel(  # type: ignore
+        cat = (
+            self.__message.guild.get_channel(category) if category else None
+        )  # type: discord.CategoryChannel # type: ignore
+        chn = await self.__message.guild.create_text_channel(  # type: ignore
             name, position=position or discord.utils.MISSING, category=cat
         )
         return chn.id
@@ -90,7 +77,7 @@ class Variables:
             perms = discord.Permissions.none()
 
         clr = discord.Color.from_str(str(color)) if color else discord.Color.default()
-        role = await self._message.guild.create_role(name=name, color=clr, permissions=perms)  # type: ignore
+        role = await self.__message.guild.create_role(name=name, color=clr, permissions=perms)  # type: ignore
         return role.id
 
 

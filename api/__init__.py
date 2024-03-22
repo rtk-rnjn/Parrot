5fd935ae272db90f0a20fba98abe7e3b267186a2
rtk-rnjn <ritik0ranjan@gmail.com>
2024-03-22 23:41:18 +0530

    Deleted OLD Apis

--- api/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .cricket_api.api import _cricket_api as cricket_api, runner
-
-__all__ = ("runner", "cricket_api")
--- api/__main__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from . import runner
-
-if __name__ == "__main__":
-    runner()
